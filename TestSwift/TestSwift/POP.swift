35ba4e2065368c67ae59c5ca11f4a656de8af8dd
beforeold <ibeforeold@gmail.com>
2022-10-11 10:07:15 +0800

    update pop comments

--- TestSwift/TestSwift/POP.swift
+++ TestSwift/TestSwift/POP.swift
@@ -14,6 +14,7 @@ protocol Ordered {
 
 // 对 Ordered 进行扩展，而不是 Comparable 的原因是不滥用，只选择性 selective
 // 也就是只在声明为 Ordered 的 Comparable 的类型，才有这个 lessThan 能力
+// 即，不是让所有的 Comparable 额外都拥有了 lessThan
 extension Ordered where Self: Comparable {
   func lessThan(_ val: Self) -> Bool {
     return self < val
@@ -36,4 +37,17 @@ extension Int: Ordered {}
 fileprivate
 func testFoo() {
   foo(5)
+}
+
+extension Comparable {
+  func lessThan(_ val: Self) -> Bool {
+    return self < val
+  }
+}
+
+fileprivate
+func testDouble() {
+  // 如果直接 extension Comparable
+  // Double 支持支持了 lessThan(_:)
+  _ = 0.5.lessThan(0.4)
 }
